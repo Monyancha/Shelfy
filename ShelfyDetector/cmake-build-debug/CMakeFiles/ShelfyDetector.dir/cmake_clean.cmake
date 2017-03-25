e37c28d1d58ac24a0d9953491c5922d7b7f4d829
Saj Arora <saj.arora24@gmail.com>
2017-03-25 14:46:43 -0400

    added c++ shelfy detector

---
+++ ShelfyDetector/cmake-build-debug/CMakeFiles/ShelfyDetector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShelfyDetector.dir/main.cpp.o"
+  "ShelfyDetector.pdb"
+  "ShelfyDetector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShelfyDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
